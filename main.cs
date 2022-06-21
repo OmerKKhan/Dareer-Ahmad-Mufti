6c5a5953a7219abb93d470953bb0162ce2728c4c
Omer Khan <omerkhan.nu@gmail.com>
2022-06-21 04:50:51 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,24 @@
+//Palindrome problem
+// A phrase is a palindrome if, it is the same string if it is reversed
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+// Example 1:
+
+// Input: s = "amanaplanacanalpanama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "raceacar"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+
+// Input: s = "cars"
+// Output: false
+
+
+bool IsPalindrome(string input)
+{
+  
+}
